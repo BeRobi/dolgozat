01f0b25f47bac8bc4a1eddc6fd99906af12820a4
BeRobi <screengsm@gmail.com>
2023-03-18 09:33:52 +0100

    3. kutya kiírása + szövegtartalom

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-window.addEventListener("load", init)
+window.addEventListener("load", init);
 
-const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
+const kutyaLista = [
+  "foxterrier",
+  "kuvasz",
+  "puli",
+  "agár",
+  "újfullandi",
+  "keverék",
+];
 
-function init(){
-    const GOMB = document.getElementsByTagName("article")[0];
-    console.log(GOMB)
-    GOMB.innerHTML += "<button>Kutya</button>"
+function init() {
+  const GOMB = document.getElementsByTagName("article")[0];
+  console.log(GOMB);
+  GOMB.innerHTML += "<button> Kutya </button>";
 
+  const GOMBKATTINT = document.querySelectorAll("article button")[0];
 
-    const GOMBKATTINT = document.querySelectorAll("article button");
-      
-        for (let index = 0; index < GOMBKATTINT.length; index++) {
-          GOMBKATTINT.innerHTML.addEventListener("click", console.log(valami))}
-          
+  GOMBKATTINT.addEventListener("click", egerkatt);
+}
 
+function egerkatt() {
+  const KUTYAKIIR = document.querySelectorAll("article")[0];
+  KUTYAKIIR.innerHTML += `${kutyaLista[2]}`;
 }
